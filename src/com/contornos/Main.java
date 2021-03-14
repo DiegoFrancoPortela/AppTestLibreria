e7b8f2644d825afa9146916f22414e6373f20488
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-03-14 23:49:44 +0100

    Por favor que funcione :( x2

---
+++ src/com/contornos/Main.java
@@ -0,0 +1,15 @@
+package com.contornos;
+
+import Libreria.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hola");
+        //unaConsola.visualiza();
+
+        //InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Chao");
+        //unaVentana.visualiza();
+
+    }
+}
